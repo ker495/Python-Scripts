1d10a27ebe464a2ac072323c9b7cfa72f5bc48cd
Corinne Haley <ker495@gmail.com>
2021-03-19 23:27:56 -0400

    Gas Mileage
    Practice script.

---
+++ Gas Mileage.py
@@ -0,0 +1,12 @@
+
+#Corinne Haley
+#Practice Script
+#Gas Mileage
+
+miles_driven = float(input('\nEnter number of miles driven: '))
+gallons_of_gas_used = float(input('Enter gallons of gas used: '))
+
+mpg = miles_driven / gallons_of_gas_used
+
+print('Miles per gallon =', mpg, end='\n\n') 
+# Miles per Gallon = 89
